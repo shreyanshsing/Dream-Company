f21c6ec5831575e23415890682cde304079d143b
unknown <shreyansh.singh@blazsoft.com>
2021-06-16 00:16:01 +0530

    URL-update

--- src/Config/URL/url.js
+++ src/Config/URL/url.js
@@ -1,3 +1,1 @@
-//export const URL ="https://dream-company-316807.uc.r.appspot.com/"
-
-export const URL = "http://localhost:8080/";
+export const URL ="https://dream-company-316807.uc.r.appspot.com/"
